6ec83dca5bd9bb0dc57b26638d5a5505bab1c173
Drizii <wojtas1478@gmail.com>
2022-12-29 17:00:57 +0100

    pierwszy commit

---
+++ src/components/MyAwesomeComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <button @click="sayHello">Say Hello</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyAwesomeComponent',
+  setup() {
+    function sayHello() {
+    const welcomeText = 'Siema, co tam?'
+        console.log(addExclamationMark(welcomeText));
+        }
+        function addExclamationMark(text) {
+            return `${text}!`
+        }
+        return {sayHello}
+  }
+}
+</script>
+
