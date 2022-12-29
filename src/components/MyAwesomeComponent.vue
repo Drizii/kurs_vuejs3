0ab7c8e3defd526a7fdddad91a2efcd3afbd870f
Drizii <wojtas1478@gmail.com>
2022-12-29 17:32:23 +0100

    let - funkcja zwiekszajaca

--- src/components/MyAwesomeComponent.vue
+++ src/components/MyAwesomeComponent.vue
@@ -1,18 +1,23 @@
 <template>
     <div>
-        <button @click="sayHello">Say Hello</button>
+        <button @click="incrementPension">Increment My Pension</button>
         <p>Your pension will be: {{ pension }}</p>
     </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
     name: 'MyAwesomeComponent',
     setup() {
+        let pension = ref(100);
 
-        let pension = 1000
+        function incrementPension() {
+            pension.value++;
+        }
 
-        return { pension }
+        return { pension, incrementPension };
     }
 }
 </script>
