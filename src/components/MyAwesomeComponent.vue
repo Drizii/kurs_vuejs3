2754bdd8900e344c7dc355c75a20a9728b3f31f7
Drizii <wojtas1478@gmail.com>
2022-12-29 17:33:01 +0100

    let - funkcja zwiekszajaca

--- src/components/MyAwesomeComponent.vue
+++ src/components/MyAwesomeComponent.vue
@@ -11,7 +11,7 @@ import { ref } from "vue";
 export default {
     name: 'MyAwesomeComponent',
     setup() {
-        let pension = ref(100);
+        const pension = ref(100);
 
         function incrementPension() {
             pension.value++;
