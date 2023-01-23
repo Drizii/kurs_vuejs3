88100a99b6d3037c855ebd3f5d6d1ba41c48ea44
Drizii <wojtas1478@gmail.com>
2023-01-23 16:27:32 +0100

    Conditional rendering v-show

--- src/components/MyAwesomeComponent.vue
+++ src/components/MyAwesomeComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <p>Masks: {{masks}}</p>
-        <p v-if="masks > 3">You can buy mask</p>
-        <p v-else-if="masks > 0 && masks <= 3">You can buy a mask, but hurry up!</p>
-        <p v-else>You can't buy a mask, it's out of stock!</p>
-        <button v-if="masks > 0" @click="buyMask">Buy a mask</button>
+        <p v-show="masks > 3">You can buy mask</p>
+        <p v-show="masks > 0 && masks <= 3">You can buy a mask, but hurry up!</p>
+        <p v-show="masks <= 0">You can't buy a mask, it's out of stock!</p>
+        <button v-show="masks > 0" @click="buyMask">Buy a mask</button>
     </div>
 </template>
 
